c0fedba590e6a9a4d71d7e64fd2dc0bf480515eb
Seba <sebacoli00@gmail.com>
2024-08-06 14:36:32 -0300

    mock data configured
    Signed-off-by: Seba <sebacoli00@gmail.com>

--- notes-app-back/src/models/noteModel.js
+++ notes-app-back/src/models/noteModel.js
@@ -3,19 +3,22 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/dbConfig');
 
 const Note = sequelize.define('Note', {
-  name: {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  title: {
     type: DataTypes.STRING,
     allowNull: false
   },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
+  content: {
+    type: DataTypes.TEXT,
     allowNull: false
   }
+}, {
+  tableName: 'Note',
+  timestamps: false
 });
 
 module.exports = Note;
--- notes-app-back/src/services/noteService.js
+++ notes-app-back/src/services/noteService.js
@@ -1,5 +1,11 @@
-const Note = require('../models/noteModel');
+const note = require('../models/noteModel');
 
 exports.getAllNotes = async () => {
-  return await true;//Note.find();
+  try {
+    const notes = await note.findAll(); // Fetch all notes from the database
+    return notes;
+  } catch (error) {
+    console.error('Error fetching notes:', error);
+    throw error;
+  }
 };
